6e432cfb4cedf666251c89483d13968c6708d538
Steven Schulte <sschulte@secc.org>
2022-01-05 16:57:41 -0500

    Update palindrome.js
    Better solution with a hash object to keep track of paired letters

--- palindrome.js
+++ palindrome.js
@@ -10,69 +10,49 @@ return True, but is not a palindrome itself
 Description: Tacocat is a palindrome
 */
 
-let permutationString = "Tact Coa";
-let palindromeString = "tacocat";
+/*
+  "taco cat" = tacocat // True
+  "atco cat" = tacocat // True
+  " rac ecar rara " = arracecarra // True
+  "chirpingmermaid" = c h p i n g e a d // False
+  "aabbc" = abcba // True 
+  "aaaabbbbcc" = aabbccbbaa // True 
+  "aabc" = abac or acab // False
+  "" = // False as an empty string
+ */
 
-// A function to see if it is a Palindrome
-const isPalindrome = function (str) {
-  let arrayOfString = str.split("");
-  let reverseString = arrayOfString.reverse().join(""); // back to string for comparison
-  if (str == reverseString) {
-      console.log(str + " is a palindrome");
-      return true;
-  } else {
-      console.log(str + " is not a palindrome");
-      return false;
-  }
-};
-
-// A function to convert strings into an obj with a count of each letters
-const objectLetterCounts = function (str) {
-  const strArray = str.toLowerCase().replace(/\s/g, '').split("");
-  const obj = {}; 
-  for (let i = 0; i < strArray.length; i++) {
-    if (strArray[i] in obj) {
-        obj[strArray[i]] = obj[strArray[i]] + 1; // add to existing count of letters
+let palPerm = (s) => {
+   let hash = {};
+   let charCount = 0;
+   let stArray = s.split('');
+   console.log(stArray);
+   stArray.forEach( (letter) => { 
+      // skip spaces
+      if (letter === ' ') {
+        return;
+      }
+      // if  we see the same letter again, delete from hash
+      if (hash[letter]) {
+        delete hash[letter];
+      } else {
+        hash[letter] = true;
+      }
+      charCount++; 
+   });  
+    // check hash at the end: odd - 1 key left, even - no keys left
+    if (charCount != 0 && charCount % 2 === 0) {
+      return Object.keys(hash).length === 0;
     } else {
-        obj[strArray[i]] = 1; // 1 for one letter
-    }
-  }
-  console.log(obj);
-  return obj;
-};
-
-// A function to see if a work is a permutation of a palindrome
-const isPermutation = function (palindromeWord, stringToCompare) {
-    const palindromeWordObj = objectLetterCounts(palindromeWord);
-    const stringToCompareObj = objectLetterCounts(stringToCompare);
-    let isPermutation = true;
-    for (const key in palindromeWordObj) {
-        if (palindromeWordObj[key] != stringToCompareObj[key]) {
-            isPermutation = false;
-        }
-    }
-    // check if the first word is a palindrome first
-    if (isPalindrome(palindromeWord)) {
-        if (isPermutation) {
-          console.log("'" + stringToCompare + "' is a permutation of the palindrome: " + palindromeWord);
-          return true;
-        } else {
-          console.log("'" + stringToCompare + "' is not a permutation of the palindrome: " + palindromeWord);
-          return false;
-        }
-    } else {
-        console.log(palindromeWord + " is not a palindrome");
-        return false;
+      return Object.keys(hash).length === 1;
     }
 };
-
-// check permutations to "Tact Coa"
-isPermutation(palindromeString, permutationString);
-
-// change permutations to "tacocat"
-permutationString = "tacocat";
-isPermutation(palindromeString, permutationString);
-
-// change permutations to "acoctta"
-permutationString = "acoctta";
-isPermutation(palindromeString, permutationString);
+console.log(
+  palPerm('taco cat'), 
+  palPerm('atco cat'),
+  palPerm(' rac ecar rara '),
+  palPerm('chirpingmermaid'),
+  palPerm('aabbc'),
+  palPerm('aaaabbbbcc'),
+  palPerm('aabc'),
+  palPerm('')
+);
